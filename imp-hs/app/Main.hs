c476d33cd1abca61775e59b30a677b61c1d8c3b6
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-03-09 13:54:00 +0000

    app/Main.hs: worknig BExp evaluator too

--- imp-hs/app/Main.hs
+++ imp-hs/app/Main.hs
@@ -41,24 +41,33 @@ data BExp = BBool Bool
 
 evalAExp :: AExp -> Memory -> AExp
 evalAExp ae@(AId aid)      m = maybe ae AInt $ HM.lookup aid m
-evalAExp ae@(ADiv   a1 a2) m = maybe ae id   $ evalBinOp a1 a2 m (/)
-evalAExp ae@(AMul   a1 a2) m = maybe ae id   $ evalBinOp a1 a2 m (*)
-evalAExp ae@(AMinus a1 a2) m = maybe ae id   $ evalBinOp a1 a2 m (-)
-evalAExp ae@(APlus  a1 a2) m = maybe ae id   $ evalBinOp a1 a2 m (+)
+evalAExp ae@(ADiv   a1 a2) m = maybe ae id   $ evalBinAExp a1 a2 m (div)
+evalAExp ae@(AMul   a1 a2) m = maybe ae id   $ evalBinAExp a1 a2 m (*)
+evalAExp ae@(AMinus a1 a2) m = maybe ae id   $ evalBinAExp a1 a2 m (-)
+evalAExp ae@(APlus  a1 a2) m = maybe ae id   $ evalBinAExp a1 a2 m (+)
 evalAExp i _ = i
 
-evalBinOp :: AExp -> AExp -> Memory -> (Int -> Int -> Int) -> Maybe AExp
-evalBinOp a1 a2 m f = case (evalAExp a1 m, evalAExp a2 m) of
-                        (AInt i1, AInt i2) -> Just $ AInt (f i1 i2)
-                        _                  -> Nothing
+evalBinAExp :: AExp -> AExp -> Memory -> (Int -> Int -> Int) -> Maybe AExp
+evalBinAExp a1 a2 m f = case (evalAExp a1 m, evalAExp a2 m) of
+                            (AInt i1, AInt i2) -> Just $ AInt (f i1 i2)
+                            _                  -> Nothing
 
 evalBExp :: BExp -> Memory -> BExp
-evalBExp (BBool b)    _ = b
-evalBExp (BLE a1 a2)  m = (evalAExp a1 m) <= (evalAExp a2 m)
-evalBExp (BLT a1 a2)  m = (evalAExp a1 m) <  (evalAExp a2 m)
-evalBExp (BEQ a1 a2)  m = (evalAExp a1 m) == (evalAExp a2 m)
-evalBExp (BNot b)     m = not (evalBExp b m)
-evalBExp (BAnd b1 b2) m = (evalBExp b1 m) && (evalBExp b2 m)
+evalBExp be@(BLE a1 a2) m = maybe be id $ (evalCompBExp a1 a2 m) (<=)
+evalBExp be@(BLT a1 a2) m = maybe be id $ (evalCompBExp a1 a2 m) (<)
+evalBExp be@(BEQ a1 a2) m = maybe be id $ (evalCompBExp a1 a2 m) (==)
+evalBExp be@(BAnd be1 be2) m = case (evalBExp be1 m, evalBExp be2 m) of
+                                (BBool b1, BBool b2) -> BBool (b1 && b2)
+                                _                    -> be
+evalBExp (BNot be) m = case evalBExp be m of
+                        BBool b -> BBool (not b)
+                        _       -> BNot be
+evalBExp b _ = b
+
+evalCompBExp :: AExp -> AExp -> Memory -> (Int -> Int -> Bool) -> Maybe BExp
+evalCompBExp ae1 ae2 m f = case (evalAExp ae1 m, evalAExp ae2 m) of
+                            (AInt a1, AInt a2) -> Just $ BBool (f a1 a2)
+                            _                  -> Nothing
 
 main :: IO ()
 main = pure ()
